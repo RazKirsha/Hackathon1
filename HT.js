197448aafa30db88682d90f665ffee04bbfefa97
Your Name <you@example.com>
2021-04-23 15:07:19 +0300

    test

--- HT.js
+++ HT.js
@@ -1,3 +1,5 @@
+// RAZ until line 90
+
 let audio_place = document.getElementsByClassName("audioFile")[0];
 let question_place = document.getElementsByClassName("question")[0];
 let answers_place = document.getElementsByClassName("option");
@@ -6,8 +8,7 @@ let next = document.getElementsByClassName("start")[0];
 
 let scores = 0;
 let index = 0;
-let questions = [
-    {
+let questions = [{
         sound: "Elephant.mp3",
         option1: "AA",
         option2: "BB",
@@ -26,63 +27,96 @@ let questions = [
 ];
 // let qst = questions[index];
 // let keys = Object.keys(qst)
-    
-function appendAudio(link){
+
+function appendAudio(link) {
     let created_audio = document.createElement("audio");
-    created_audio.setAttribute("controls",'');
+    created_audio.setAttribute("controls", '');
     let created_source = document.createElement("source");
-    created_source.setAttribute("src",link);
-    created_source.setAttribute("type","audio/ogg");
+    created_source.setAttribute("src", link);
+    created_source.setAttribute("type", "audio/ogg");
     created_audio.appendChild(created_source);
     audio_place.appendChild(created_audio);
 }
 
-function setOptions(){
-    for (let i=0;i<4;i++){
-        let obj_place = keys[i+1];
+function setOptions() {
+    for (let i = 0; i < 4; i++) {
+        let obj_place = keys[i + 1];
         answers_place[i].innerHTML = qst[obj_place];
     }
 }
 
-function getAnswer(){
-    for (ap of answers_place){
-        ap.addEventListener("click",function (e){
-            if (qst.answer == e.target.innerHTML){
+function getAnswer() {
+    for (ap of answers_place) {
+        ap.addEventListener("click", function (e) {
+            if (qst.answer == e.target.innerHTML) {
                 this.style.backgroundColor = "green";
                 scores += 100;
-                index+=1;
+                index += 1;
             } else {
                 this.style.backgroundColor = "red";
-                index+=1;
+                index += 1;
             }
         })
     }
 }
 
-for (let j=0;j<questions.length;j++){
+for (let j = 0; j < questions.length; j++) {
     let qst = questions[j];
     let keys = Object.keys(qst);
 
     appendAudio(qst.sound);
 
-    
-    for (ap of answers_place){
 
-        ap.addEventListener("click",function (e){
-            for (let i=0;i<4;i++){
-                let obj_place = keys[i+1];
+    for (ap of answers_place) {
+
+        ap.addEventListener("click", function (e) {
+            for (let i = 0; i < 4; i++) {
+                let obj_place = keys[i + 1];
                 answers_place[i].innerHTML = qst[obj_place];
             }
-            if (qst.answer == e.target.innerHTML){
+            if (qst.answer == e.target.innerHTML) {
                 this.style.backgroundColor = "green";
                 scores += 100;
-                index+=1;
+                index += 1;
             } else {
                 this.style.backgroundColor = "red";
-                index+=1;
+                index += 1;
             }
         })
     }
 
     // console.log(qst.sound);
 }
+// RAZ
+
+// SHY  until line 122
+var myscore = document.getElementById('next');
+var zero = document.getElementById('counter');
+myscore.addEventListener('click', myfunction)
+
+function myfunction() {
+    zero.innerHTML = +(zero.innerHTML) + 100;
+}
+
+// timer:
+document.addEventListener('DOMContentLoaded', () => {
+    const timeLeftDisplay = document.querySelector('.timer')
+    const startBtn = document.querySelector('.button')
+    let timeLeft = 60
+
+    function countDown() {
+
+        setInterval(function () {
+            if (timeLeft <= 0) {
+                clearInterval(timeLeft = 0);
+            }
+            timeLeftDisplay.innerHTML = "TIME: " + timeLeft;
+            timeLeft -= 1;
+        }, 1000);
+
+    }
+    startBtn.addEventListener('click', countDown);
+
+})
+
+// shy
